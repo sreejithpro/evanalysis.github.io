70f1d0b0ad3d51c8735d232ab1dc5811a4e03d3b
sreejithpro <sreejithpro@gmail.com>
2023-08-02 15:27:20 +0530

    Update form.js

--- form.js
+++ form.js
@@ -3,33 +3,31 @@ document.getElementById("costBenefitForm").addEventListener("submit", function(e
 
   // Get form values
   const vehicleType = document.querySelector('input[name="vehicle-type"]:checked').value;
-  const vehiclePrice = parseFloat(document.getElementById("vehicle-price").value);
+  const vehicle_price_ICE = parseFloat(document.getElementById("vehicle-price_ICE").value);
+  const vehicle_price_EV = parseFloat(document.getElementById("vehicle-price_EV").value);
   const mileageYear = parseFloat(document.getElementById("mileage-year").value);
-  const fuelCost = parseFloat(document.getElementById("fuel-cost").value);
+  const fuelCost_ICE = parseFloat(document.getElementById("fuel-cost_ICE").value);
   const electricityCost = parseFloat(document.getElementById("electricity-cost").value);
-  const maintenanceCharges = parseFloat(document.getElementById("maintenance-charges").value);
+  const ICE_mileage = parseFloat(document.getElementById("ICE_mileage").value);
+  const EV_range = parseFloat(document.getElementById("EV_range").value);
+  const EV_capacity = parseFloat(document.getElementById("EV_capacity").value);
 
   // Perform cost-benefit analysis
-  let totalCost = vehiclePrice;
-  let totalBenefit = 0;
-
-  if (vehicleType === "two-wheeler") {
-    totalCost += (mileageYear / fuelCost) * mileageYear * fuelCost;
-  } else {
-    totalCost += (mileageYear / fuelCost) * mileageYear * fuelCost;
-  }
-
-  totalCost += (mileageYear / fuelCost) * mileageYear * fuelCost;
-  totalCost += (mileageYear / fuelCost) * mileageYear * fuelCost;
-  totalCost += maintenanceCharges;
+	let fuel_ICE = (mileageYear/ICE_mileage)*fuelCost_ICE
+  let fuel_EV = (mileageYear/EV_range)*EV_capacity*electricityCost
+  let BEY = (vehicle_price_EV-vehicle_price_ICE)/(fuel_ICE-fuel_EV)
+  
 
   // Display the results
   const resultContainer = document.getElementById("result");
   resultContainer.style.display = "block";
 
-  const totalCostElement = document.getElementById("totalCost");
-  totalCostElement.textContent = `Total Cost of Ownership: $${totalCost.toFixed(2)}`;
+  const fuel_ICEr = document.getElementById("fuel_ICEd");
+  fuel_ICEr.textContent = `Cost of Fuel ICE is: ${fuel_ICE.toFixed(2)}`;
 
-  const totalBenefitElement = document.getElementById("totalBenefit");
-  totalBenefitElement.textContent = `Total Benefit: $${totalBenefit.toFixed(2)}`;
+  const fuel_EVr = document.getElementById("fuel_EVd");
+  fuel_EVr.textContent = `Cost of Electricity: ${fuel_EV.toFixed(2)}`;
+  
+  const BEYr = document.getElementById("BEYd");
+  BEYr.textContent = `Year to Break Even: ${BEY.toFixed(1)}`;
 });
